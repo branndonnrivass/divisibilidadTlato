40f8a4ecc990093f4661b2a463a2e3fceea30c61
Braulio-tron300 <112912534+Braulio-tron300@users.noreply.github.com>
2026-06-01 23:46:58 -0600

    Algorimo de euclides
    ya en funcion y estrucuras

--- divisivilidad.c
+++ divisivilidad.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct 
+{
+   int q;
+   int r;
+}Div;
+
+Div divicion(int a, int b);
+void Euclides(int a, int b);
 
 int main()
 {
@@ -9,64 +18,95 @@ int main()
     scanf("%d",&a);
     printf("ingresar divisor:");
     scanf("%d",&b);
-    r=abs(a);
+    Div d2 ;
+    
+    
+    d2= divicion(a,b);
+
+
+    printf(" %d=%d * %d + %d",a,b,d2.q,d2.r);
+    Euclides(a,b);
+        return 0;
+}
+
+
+Div divicion(int a , int b ){
+    Div d1;
+    d1.q=0;
+
+    d1.r=abs(a);
     if((a>=0) &&(b>0))  {
-        while ( r >= abs(b))
+        while ( d1.r >= abs(b))
         {
         
-            r=r-abs(b);
+            d1.r=d1.r-abs(b);
 
-            q++;
+            d1.q++;
         }
     }
     if((a>=0) &&(b<0))  {
-        while ( r >= abs(b))
+        while ( d1.r >= abs(b))
         {
         
-            r=r-abs(b);
+            d1.r=d1.r-abs(b);
 
-            q++;
+            d1.q++;
         }
-        q=-q;
+        d1.q=-d1.q;
     }
 
     if((a<0) &&(b>0))  {
-        while ( r >= abs(b))
+        while ( d1.r >= abs(b))
         {
         
-            r=r-abs(b);
+            d1.r=d1.r-abs(b);
 
-            q++;
+            d1.q++;
         }
-        if (r!=0)
+        if (d1.r!=0)
         {
-           q = -(q + 1);
-            r = b - r;
+           d1.q = -(d1.q + 1);
+            d1.r = b - d1.r;
         }
         else
-            q=-q;
+            d1.q=-d1.q;
         
     }
 
       if((a<0) &&(b<0))  {
-        while ( r >= abs(b))
+        while ( d1.r >= abs(b))
         {
         
-            r=r-abs(b);
+            d1.r=d1.r-abs(b);
 
-            q++;
+            d1.q++;
         }
-        if (r!=0)
+        if (d1.r!=0)
         {
-           q ++;
-            r = ((-b) - r);
+           d1.q ++;
+            d1.r = ((-b) - d1.r);
         }
         else
-            q=-q;
+            d1.q=-d1.q;
         
     }
+ return d1;
+}
 
 
-    printf(" %d=%d * %d + %d",a,b,q,r);
-        return 0;
+
+void Euclides(int a, int b){
+while (b!=0)
+{
+ Div d ;
+
+d=divicion(a,b);
+a=b;
+b=d.r;
+}
+printf("\n Maximo comun divisor : %d",a);
+
+
+
+
 }
